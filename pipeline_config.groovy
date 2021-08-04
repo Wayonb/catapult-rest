e8e90dee074a23e79d0851faf2635c4e47c3b8b0
wayonb <wayonb@gmail.com>
2021-08-03 22:19:06 -0400

    update generic build

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,7 +5,7 @@
 */
 libraries{
 
-    @override generic_build {
+    generic_build {
         tests = ["unit_test.sh catapult-sdk", "unit_test.sh rest", "unit_test.sh spammer"]
     }
     publish_artifacts {
