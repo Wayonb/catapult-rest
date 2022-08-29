fa6f580b6c96aca7c77135e25539832d1e7eb366
wayonb <wayon@symbol.dev>
2022-08-29 01:57:45 -0400

    test

--- jenkinsfiles/Jenkinsfile-nightly.groovy
+++ jenkinsfiles/Jenkinsfile-nightly.groovy
@@ -1,9 +1,10 @@
-import hudson.model.*
+
+String shouldPublishFailJobStatusName = 'SHOULD_PUBLISH_FAIL_JOB_STATUS'
 
 pipeline {
 	parameters {
-		string name: 'JOB_BRANCH', description: 'branch to trigger jobs', defaultValue: 'dev'
-		booleanParam name: 'SHOULD_PUBLISH_FAIL_JOB_STATUS', description: 'true to publish job status if failed', defaultValue: true
+		string name: 'JOB_BRANCH_NAME', description: 'branch to trigger jobs', defaultValue: 'dev'
+		booleanParam name: shouldPublishFailJobStatusName, description: 'true to publish job status if failed', defaultValue: true
 		booleanParam name: 'WAIT_FOR_BUILDS', description: 'true to wait for trigger build to complete', defaultValue: true
 	}
 
@@ -12,8 +13,7 @@ pipeline {
 	}
 
 	triggers {
-		//cron('@midnight')
-		cron('H/5 * * * *')
+		cron('@midnight')
 	}
 
 	options {
@@ -25,13 +25,12 @@ pipeline {
 		stage('display environment') {
 			steps {
 				sh 'printenv'
-				sh 'ls -la'
 			}
 		}
 		stage('trigger build jobs') {
 			steps {
 				script {
-					triggerAllJobs(env.JOB_BRANCH, env.WAIT_FOR_BUILDS ? env.WAIT_FOR_BUILDS.toBoolean() : true)
+					triggerAllJobs(env.JOB_BRANCH_NAME, env.WAIT_FOR_BUILDS ? env.WAIT_FOR_BUILDS.toBoolean() : true)
 				}
 			}
 		}
@@ -40,7 +39,7 @@ pipeline {
 		unsuccessful {
 			script {
 				if (null != env.SHOULD_PUBLISH_FAIL_JOB_STATUS && env.SHOULD_PUBLISH_FAIL_JOB_STATUS.toBoolean()) {
-					helper.sendDiscordNotification (
+					helper.sendDiscordNotification(
 						"Jenkins Job Failed for ${currentBuild.fullDisplayName}",
 						"At least one job failed for Build#${env.BUILD_NUMBER} which has a result of ${currentBuild.currentResult}.",
 						env.BUILD_URL,
@@ -53,17 +52,10 @@ pipeline {
 }
 
 void triggerAllJobs(String branchName, boolean waitForDownStream) {
-	// final String pathSeparator = '/'
-	// String filepath = helper.resolveBuildConfigurationFile()
-
-	// Map<String, String>  jobInfos = [:]
-	// yamlHelper.readYamlFromFile(filepath).builds.each { 
-	// 	jobInfos.put(it.path.tokenize(pathSeparator).last(), it.name)
-	// }
-
 	Map<String, String> siblingNameMap = siblingJobNames()
-
 	Map<String, Closure> buildJobs = [:]
+	final String platformName = 'PLATFORM'
+
 	siblingNameMap.each { siblingName ->
 		String displayName = siblingName.value
 		buildJobs["${displayName}"] = {
@@ -71,15 +63,15 @@ void triggerAllJobs(String branchName, boolean waitForDownStream) {
 				String fullJobName = siblingName.key + '/' + branchName
 
 				// Platform parameter can vary per project, get the last value
-				String platform = getLastJobParameter(fullJobName, 'PLATFORM')
+				String platform = lastJobParameterValue(fullJobName, platformName)
 
 				build job: "${fullJobName}", parameters: [
 					gitParameter(name: 'MANUAL_GIT_BRANCH', value: branchName),
-					string(name: 'PLATFORM', value: platform ?: 'ubuntu'),
+					string(name: platformName, value: platform ?: 'ubuntu'),
 					string(name: 'BUILD_CONFIGURATION', value: 'release-private'),
 					string(name: 'TEST_MODE', value: 'code-coverage'),
 					booleanParam(name: 'SHOULD_PUBLISH_IMAGE', value: false),
-					booleanParam(name: 'SHOULD_PUBLISH_FAIL_JOB_STATUS', value: SHOULD_PUBLISH_FAIL_JOB_STATUS.toBoolean())],
+					booleanParam(name: shouldPublishFailJobStatusName, value: env.SHOULD_PUBLISH_FAIL_JOB_STATUS.toBoolean())],
 					wait: waitForDownStream
 			}
 		}
@@ -90,22 +82,22 @@ void triggerAllJobs(String branchName, boolean waitForDownStream) {
 
 Map<String, String> siblingJobNames() {
 	Item project = Jenkins.get().getItemByFullName(currentBuild.fullProjectName)
-	List<Items> siblingItems = project.parent.getItems()
+	List<Items> siblingItems = project.parent.items
 
 	Map<String, String> targets = [:]
 	for (item in siblingItems) {
-		echo "found item - ${item}"
-		if (!item instanceof AbstractModelObject || item.fullName == project.fullName) {
+		if (item !instanceof AbstractModelObject || item.fullName == project.fullName) {
 			continue
 		}
+
 		targets.put(item.fullName, item.displayName)
 	}
 
 	return targets
 }
 
-Object getLastJobParameter(String fullJobName, String parameterName) {
+Object lastJobParameterValue(String fullJobName, String parameterName) {
 	Item job = Jenkins.get().getItemByFullName(fullJobName)
-	ParametersAction params = job.getLastBuild().getAction(hudson.model.ParametersAction)
-	return params.getParameter(parameterName).getValue()
+	ParametersAction params = job.lastBuild.getAction(hudson.model.ParametersAction)
+	return params.getParameter(parameterName).value()
 }
